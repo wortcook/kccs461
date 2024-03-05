8c595a88dbfbff6b62d46f731622d12c4a4b9029
wort <wortcook@gmail.com>
2024-03-05 10:02:49 -0600

    ongoing work

--- hw01/src/edu/umkc/cs461/hw1/data/BiDirectGraph.java
+++ hw01/src/edu/umkc/cs461/hw1/data/BiDirectGraph.java
@@ -5,16 +5,44 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
+import com.google.common.collect.BiMap;
 
-public class BiDirectGraph<K extends Comparable<K>>{
-    final ArrayList<K> nodes = new ArrayList<K>();
+
+
+public class BiDirectGraph<K extends Comparable<K> & Measureable<K>>{
+    final BiMap<K, Integer> nodeMap = new HashMap<K, Integer>();
+    final BiMap<Integer, K> reverseNodeMap = new HashMap<Integer, K>();
     double[][] edges;
 
-    protected BiDirectGraph(final List<K> nodes, final double[][] edges){
-        ;
+    //We assume nodes is sorted
+    protected BiDirectGraph(final  Map<K, Integer> nodeMap, final double[][] edges){
+        this.nodeMap.putAll(nodeMap);
+        this.edges = edges;
     }
 
-    public static class BiDirectGraphBuilder<K extends Comparable<K>>{
+    public Map<K,Double> getConnections(final K node){
+        int index = this.nodeMap.get(node);
+        Map<K,Double> ret = new HashMap<K,Double>();
+
+        double[] indexRow = edges[index];
+        for(int i = 0 ; i < indexRow.length; i++){
+            if(indexRow[i] != Double.POSITIVE_INFINITY){
+                ret.put( this.reverseNodeMap.get(i), indexRow[i]);
+            }
+        }
+
+        for(int i = index + 1; i < edges.length; i++){
+            if(edges[i][index] != Double.POSITIVE_INFINITY){
+                ret.put( this.reverseNodeMap.get(i), edges[i][index]);
+            }
+        }
+
+        return ret;
+    }
+
+
+
+    public static class BiDirectGraphBuilder<K extends Comparable<K> & Measureable<K>>{
 
         private ArrayList<K> toAdd = new ArrayList<K>();
         private List<NodePair<K>> edges = new ArrayList<NodePair<K>>();
@@ -44,19 +72,15 @@ public class BiDirectGraph<K extends Comparable<K>>{
         }
 
         public BiDirectGraph<K> build(){
-
-            BiDirectGraph<K> graph = null;
-
             toAdd.sort(null);
 
+            //Puts the list of nodes so we can map city name to array position
             Map<K, Integer> nodeMap = new HashMap<K, Integer>();
 
             //the following auto suggested by co-pilot and slightly edited
             for(int i = 0; i < toAdd.size(); i++){
                 nodeMap.put(toAdd.get(i), i);
             }
-            
-            
 
             //the following auto suggested by co-pilot and slightly edited
             //by me to add the pyramid sizing of the array
@@ -70,11 +94,33 @@ public class BiDirectGraph<K extends Comparable<K>>{
             for(int i = 1; i < toAdd.size(); i++){
                 edgeArray[i] = new double[i];
                 for(int j = 0; j < i; j++){
+                    //Disconnected nodes are infinity away
                     edgeArray[i][j] = Double.POSITIVE_INFINITY;
                 }
             }
 
-            return graph;
+            //Now handle the edges and set the distances
+            for(NodePair<K> edge : edges){
+                int a = nodeMap.get(edge.getANode());
+                int b = nodeMap.get(edge.getBNode());
+
+                //We need to swap so that a is the smaller number
+                //we make the assumption here that the nodes are
+                //comparable and that the comparison is consistent
+                if(b > a){
+                    int temp = a;
+                    a = b;
+                    b = temp;
+                }
+
+                double distance = edge.getANode().distanceFrom(edge.getBNode());
+
+                //Set the distance, we only set one side of the distances
+                //since the graph is bi-directional
+                edgeArray[a][b] = distance;
+            }
+
+            return new BiDirectGraph<>(nodeMap, edgeArray);
         }
 
     }
---
+++ hw01/src/edu/umkc/cs461/hw1/data/Measureable.java
@@ -0,0 +1,5 @@
+package edu.umkc.cs461.hw1.data;
+
+public interface Measureable<K> {
+    public double distanceFrom(K other);
+}
